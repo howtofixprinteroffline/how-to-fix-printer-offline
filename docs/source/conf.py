9792bad61f947e638e20e9b7759c7cc7b2bd1a3a
Tison leeson <tisonleeson@gmail.com>
2026-03-09 17:35:27 +0530

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -10,7 +10,11 @@ html_js_files = [
     'https://code.jivosite.com/widget/cTI6R8IfQH'
 ]
 
+# conf.py
 
+html_meta = {
+    "google-site-verification": "cbJXUbg3q6lAdJ8EHe_S1IrpeWpZqs_DNsdJe9W2UxY"
+}
 
 project = 'Lumache'
 copyright = '2021, Graziella'
