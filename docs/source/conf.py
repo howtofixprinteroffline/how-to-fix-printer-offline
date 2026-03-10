ff50b960a127452578b8693696d60d847722149b
Tison leeson <tisonleeson@gmail.com>
2026-03-10 11:13:25 +0530

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -9,6 +9,7 @@ html_theme = 'alabaster'
 html_meta = {
     "google-site-verification": "cbJXUbg3q6lAdJ8EHe_S1IrpeWpZqs_DNsdJe9W2UxY"
 }
+<meta name="google-site-verification" content="cbJXUbg3q6lAdJ8EHe_S1IrpeWpZqs_DNsdJe9W2UxY" />
 
 html_js_files = [
     'https://code.jivosite.com/widget/cTI6R8IfQH'
